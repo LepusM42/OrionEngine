c7c0fdb9be0124d0b65b715d37c8d00825eb7577
LepusM42 <lepusm42@gmail.com>
2021-06-17 21:26:36 -0700

    Birth of Betelgeuse
    I've hit a slump for a while, but I finally got Betel to a point that I can start testing it. I'll likely move its classes to Betel.cpp, so users can't access its internal workings. I want Betel's available interface to be small, since I don't need (or want) users to do much with it, aside from maybe some initial configuration to suit their needs.

---
+++ Source/Betel.hpp
@@ -0,0 +1,128 @@
+/******************************************************************************!
+* \file Betel.hpp
+* \author Lepus
+* \brief The Betelgeuse Memory Manager (hereafter called Betel) is a custom
+*  memory management system for C++ applications.
+*******************************************************************************/
+#pragma once
+namespace Orion
+{
+	namespace Betel
+	{
+		template <typename ObjectType>
+		ObjectType* Allocate(unsigned count = 1);
+		void Deallocate(void* address);
+	}
+
+	namespace BetelInterior
+	{
+		/**********************************************************************!
+		* \class
+		* \brief
+		***********************************************************************/
+		class Block
+		{
+		public:
+			//! Does nothing.
+			Block(char* address, unsigned size);
+			//! Does nothing.
+			void Split(unsigned size);
+			//! Does nothing.
+			Block* Next();
+			//! Does nothing.
+			char* Address();
+			//! Does nothing.
+			void SetNext(Block* block);
+			//! Does nothing.
+			void Cut();
+		private:
+			//! Does nothing.
+			Block* m_next{ nullptr };
+			//! Does nothing.
+			Block* m_previous{ nullptr };
+			//! Does nothing.
+			char* m_address{ nullptr };
+			//! Does nothing.
+			unsigned m_size{ 0 };
+		};
+
+		/**********************************************************************!
+		* \class
+		* \brief
+		***********************************************************************/
+		class Page
+		{
+		public:
+			//! Does nothing.
+			Page(unsigned max, unsigned blockSize);
+			//! Does nothing.
+			~Page();
+			//! Does nothing.
+			void* Allocate(unsigned size);
+			//! Does nothing.
+			void Deallocate(void* address);
+			//! Does nothing.
+			Page* Next();
+			//! Does nothing.
+			bool Invalid(unsigned size);
+			//! Does nothing.
+			bool Contains(void* address);
+			//! Does nothing.
+			void AddPage();
+		private:
+			//! Does nothing.
+			void AdvanceList(Block*& blockList);
+			//! Does nothing.
+			void AddToList(Block*& blockList, Block* block);
+			//! Does nothing.
+			Block* BlockContaining(void* address);
+
+			//! Does nothing.
+			void DestroyList(Block*& blockList);
+
+			//! Does nothing.
+			Page* m_next{ nullptr };
+			//! Does nothing.
+			Block* m_freeList{ nullptr };
+			//! Does nothing.
+			Block* m_usedList{ nullptr };
+			//! Does nothing.
+			unsigned m_blockSize{ 0 };
+			//! Does nothing.
+			unsigned m_pageSize{ 0 };
+			//! Does nothing.
+			char* m_rawMemory{ nullptr };
+		};
+
+		/**********************************************************************!
+		* \class
+		* \brief
+		***********************************************************************/
+		class Allocator
+		{
+		public:
+			//! Does nothing.
+			Allocator(unsigned max, unsigned min = 8);
+			//! Does nothing.
+			void* Allocate(unsigned size);
+			//! Does nothing.
+			void Deallocate(void* address);
+			//! Does nothing.
+			~Allocator();
+		private:
+			//! Does nothing.
+			Page* FirstAvailablePage(unsigned size);
+			//! Does nothing.
+			Page* PageContaining(void* address);
+
+			//! Does nothing.
+			void DestroyPages();
+			//! Does nothing.
+			Page* m_pageList{ nullptr };
+			//! Does nothing.
+			unsigned m_maxSize{ 0 };
+			//! Does nothing.
+			unsigned m_minSize{ 0 };
+		};
+	}
+}
